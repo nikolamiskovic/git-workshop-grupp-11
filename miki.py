c063ab31e994503011aa3d1dc28e12dcb955ece9
sixtenfriberg <sixten.friberg@gmail.com>
2025-03-27 10:36:21 +0100

    hhej

--- miki.py
+++ miki.py
@@ -3,16 +3,12 @@ def get_age():
     age = input("Hur gammal är du hehe? ")
     print(f"{age} e riktigt duktig ålder")
 
-get_age()
 
 def get_food():
     food = input("Vad är din favoritmat ")
     print(f"Ah {food} är riktigt gott!")
 
-get_food()
 
 def get_drink():
     drink = input("Vad är din favoritdrink ")
     print(f"Ah {drink} är riktigt gott!")
-
-get_drink()
