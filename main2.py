b95b313f55709720aacd824c1f9a3e75053f4f95
sixtenfriberg <sixten.friberg@gmail.com>
2025-03-27 10:18:20 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,6 +1,12 @@
 """get_name = input("Skriv ditt namn: ")
 print(f"Tjena på dig {get_name}")
 """
-import miki
+"""import miki
 
-miki.get_age()
+miki.get_age()"""
+
+def get_food():
+    food = input("Vad är din minst favoritmat ")
+    print(f"Ah {food} är riktigt äckligt!")
+
+get_food()
