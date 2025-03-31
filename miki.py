765cb265227638344a1ca7e595f3aff668d1df96
nikolamiskovic <nikola3miskovic@icloud.com>
2025-03-31 19:57:04 +0200

    Update miki.py

--- miki.py
+++ miki.py
@@ -1,4 +1,31 @@
-def get_age():
+#---Uppkoppling mot API test---
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+
+    else:
+        print(f"Failed to retrieve data")
+
+pokemon_name = "charizard"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"]}")
+    print(f"Id: {pokemon_info["id"]}")
+    print(f"Height: {pokemon_info["height"]}")
+    print(f"Weight: {pokemon_info["weight"]}")
+
+
+
+"""def get_age():
 
     age = input("Hur gammal är du hehe? ")
     print(f"{age} e riktigt duktig ålder")
@@ -11,4 +38,4 @@ def get_food():
 
 def get_drink():
     drink = input("Vad är din favoritdrink ")
-    print(f"Ah {drink} är riktigt gott!")
+    print(f"Ah {drink} är riktigt gott!")"""
